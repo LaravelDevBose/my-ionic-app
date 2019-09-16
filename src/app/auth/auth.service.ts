0ae98c402cb7a41b8474f721c350ecd47a116703
tokingfisher19 <tokingfisher19@gmail.com>
2019-09-16 12:53:12 +0600

    Login from and auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Subject} from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+
+export class AuthService {
+    constructor(private http: HttpClient) {}
+}
