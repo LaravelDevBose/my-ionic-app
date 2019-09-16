766a3817e38093fcd6ba2067430dcf57a5d92a20
tokingfisher19 <tokingfisher19@gmail.com>
2019-09-16 16:02:34 +0600

    Login setup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,16 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Subject} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 
 export class AuthService {
+    private LOGIN_URL = '';
     constructor(private http: HttpClient) {}
+    login(email, password) {
+        this.http.post<any>(this.LOGIN_URL, { email, password })
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  email = '';
+  password = '';
+  constructor(private auth: AuthService) { }
+  formData: {
+    email: '',
+    password: ''
+  };
   ngOnInit() {}
+  userLoginSubmit() {
+    console.log('login btn press');
+    console.log(this.email);
+    console.log(this.password);
+    this.auth.login(this.email, this.password);
+  }
 
 }
